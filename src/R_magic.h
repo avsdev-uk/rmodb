b3ae6035d205e385c3ee74476bddaa236d0eaebc
avsdev-cw <craig@avsdev.uk>
2020-09-15 15:15:07 +0100

    Minor syntactic sugar

--- src/R_magic.h
+++ src/R_magic.h
@@ -1,32 +1,31 @@
-#ifndef __R_MAGIC_H__
-#define __R_MAGIC_H__
+#ifndef H__R_MAGIC__
+#define H__R_MAGIC__
 
 #include <stdio.h>
 
 #define R_STREAM_VERSION 3
 
-#define R_MAGIC_ASCII_V3   3001
-#define R_MAGIC_BINARY_V3  3002
-#define R_MAGIC_XDR_V3     3003
-#define R_MAGIC_ASCII_V2   2001
-#define R_MAGIC_BINARY_V2  2002
-#define R_MAGIC_XDR_V2     2003
-#define R_MAGIC_ASCII_V1   1001
-#define R_MAGIC_BINARY_V1  1002
-#define R_MAGIC_XDR_V1     1003
-#define R_MAGIC_EMPTY      999
-#define R_MAGIC_CORRUPT    998
+#define R_MAGIC_ASCII_V3    3001
+#define R_MAGIC_BINARY_V3   3002
+#define R_MAGIC_XDR_V3      3003
+#define R_MAGIC_ASCII_V2    2001
+#define R_MAGIC_BINARY_V2   2002
+#define R_MAGIC_XDR_V2      2003
+#define R_MAGIC_ASCII_V1    1001
+#define R_MAGIC_BINARY_V1   1002
+#define R_MAGIC_XDR_V1      1003
+#define R_MAGIC_EMPTY        999
+#define R_MAGIC_CORRUPT      998
 #define R_MAGIC_MAYBE_TOONEW 997
 
 /* pre-1 formats (R < 0.99.0) */
-#define R_MAGIC_BINARY 1975
-#define R_MAGIC_ASCII  1976
-#define R_MAGIC_XDR    1977
+#define R_MAGIC_BINARY           1975
+#define R_MAGIC_ASCII            1976
+#define R_MAGIC_XDR              1977
 #define R_MAGIC_BINARY_VERSION16 1971
 #define R_MAGIC_ASCII_VERSION16  1972
 
 int R_WriteMagic(FILE *fp, int number);
-
 int R_ReadMagic(FILE *fp);
 
-#endif // __R_MAGIC_H__
+#endif // H__R_MAGIC__
--- src/R_memory-object.h
+++ src/R_memory-object.h
@@ -1,21 +1,21 @@
-#ifndef __R_MEMORY_OBJECT_H__
-#define __R_MEMORY_OBJECT_H__
+#ifndef H__R_MEMORY_OBJECT__
+#define H__R_MEMORY_OBJECT__
 
 #include <Rinternals.h>
 
-struct rmemoryobject_t {
+struct r_memoryobject_t {
   char *buf;
-  size_t bufsz;
+  size_t buf_size;
   int magic;
 };
 
-int rdataToMemory(const char *filename, struct rmemoryobject_t *objMem);
-int memoryToRData(const char *filename, struct rmemoryobject_t objMem);
+int rdataToMemory(const char *filename, struct r_memoryobject_t *obj_mem);
+int memoryToRData(const char *filename, struct r_memoryobject_t obj_mem);
 
-SEXP memoryToRObject(struct rmemoryobject_t objMem);
-struct rmemoryobject_t *robjectToMemory(SEXP rObj, int magic);
+SEXP memoryToRObject(struct r_memoryobject_t obj_mem);
+struct r_memoryobject_t *robjectToMemory(SEXP r_obj, int magic);
 
 SEXP rdataToRObject(const char *filename);
-int robjectToRData(const char *filename, SEXP rObj, int magic);
+int robjectToRData(const char *filename, SEXP r_obj, int magic);
 
-#endif // __R_MEMORY_OBJECT_H__
+#endif // H__R_MEMORY_OBJECT__
